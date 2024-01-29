336050696132077c0e5562b51804351b42cf90ed
Nandiehakuna <gakiikelly403@gmail.com>
2024-01-29 19:13:29 +0300

    taco website localhost started successfully

---
+++ src/test/java/com/example/tacotuesday/TacoTuesdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacotuesday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoTuesdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
